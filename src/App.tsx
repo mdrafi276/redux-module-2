42789c76cd9512a07940acb88681c2aefafd6c56
mdrafi276 <mdrafi37473@gmail.com>
2024-06-25 12:20:52 +0600

    add

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className=" flex h-screen w-full justify-center items-center border-2  bg-black">
+        <button className="px-3 py-2 rounded-md bg-green-500 text-white text-xl font-semibold">Increment</button>
+        <h1 className="mx-14 text-white text-3xl">0</h1>
+        <button className="px-3 py-2 rounded-md bg-red-500 text-xl font-semibold"> Decrement</button>
+      </div>
+    </>
+  )
+}
+
+export default App
