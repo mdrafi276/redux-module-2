432a80fbad91095799b3418d30795a075adf73be
mdrafi276 <mdrafi37473@gmail.com>
2024-06-25 13:18:58 +0600

    update

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ function App() {
   return (
     <>
       <div className=" flex h-screen w-full justify-center items-center border-2  bg-black">
-        <button className="px-3 py-2 rounded-md bg-green-500 text-white text-xl font-semibold">Increment</button>
+        <button className="px-3 btn  py-2 rounded-md bg-green-500 text-white text-xl font-semibold">Increment</button>
         <h1 className="mx-14 text-white text-3xl">0</h1>
-        <button className="px-3 py-2 rounded-md bg-red-500 text-xl font-semibold"> Decrement</button>
+        <button className="px-3 btn py-2 focus:scale-95 rounded-md bg-red-500 text-xl font-semibold"> Decrement</button>
       </div>
     </>
   )
---
+++ src/redux/features/counterSlice.tsx
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+initialState: { count: 0 };
+
+const cournterSlice = createSlice({
+    name: 'counter',
+    reducers: {
+
+    }
+})
